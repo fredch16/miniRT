934611c17d02d2afda4c654ff4e291dd905b9528
Arina Tyurina <atyurina@student.42london.com>
2024-06-11 17:20:23 +0100

    Computing the normal on a transformed sphere (scaling + rotation z)

--- src/rtx.c
+++ src/rtx.c
@@ -21,8 +21,9 @@ int	main(void)
 	// t_tuple	ray_p;
 	// t_tuple	ray_dir;
 	//t_world w;
-	t_matrix	tr;
+//	t_matrix	tr;
 	t_matrix	scl;
+	t_matrix	rot;
 	t_tuple		point;
 	t_tuple		normal;
 
@@ -31,14 +32,15 @@ int	main(void)
 //	ray_create(&fake_ray, ray_p, ray_dir);
 //	ray_print(&fake_ray);
 
-	matrix_set_translation(&tr, 0, 1, 0);
-	matrix_set_4(&scl);
-	point = tuple_poi(0, 1.70711, -0.70711);
+//	matrix_set_translation(&tr, 0, 1, 0);
+	matrix_set_scaling(&scl, 1, 0.5, 1);
+	matrix_set_rotation_z(&rot, PI / 5);
+	point = tuple_poi(0, (sqrt(2)) / 2, -(sqrt(2)) / 2);
 	obj_list = NULL;
 	obj = obj_create(OT_SPHERE);
 	obj_add_back(&obj_list, obj);
 	//w.obj_list = &obj;
-	sphere_set_transform(obj, &tr, &scl);
+	sphere_set_transform(obj, &scl, &rot);
 	normal = sphere_normal_at(obj, &point);
 	tuple_print(normal);
 	//init_mlx(&data, &w);

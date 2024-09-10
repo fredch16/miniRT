95afbd6c450f2fe990b1f30232aed4c11d019a0d
Frederick Charbonnier <fcharbon@c2r3s8.42london.com>
2024-09-10 11:48:03 +0100

    started the integration of the parser with the main function, to be cleaned up of course

--- src/rtx.c
+++ src/rtx.c
@@ -6,7 +6,7 @@
 /*   By: atyurina <atyurina@student.42london.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/20 16:16:31 by fcharbon          #+#    #+#             */
-/*   Updated: 2024/09/05 17:50:39 by atyurina         ###   ########.fr       */
+/*   Updated: 2024/09/10 11:46:57 by fcharbon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,6 +44,9 @@ void	free_objects_memory(t_parser *parser)
 
 int	main(int argc, char **argv)
 {
+	t_world w;
+	t_data	data;
+
 	char		**scene;
 	t_parser	parser;
 
@@ -68,5 +71,25 @@ int	main(int argc, char **argv)
 		free_text(scene);
 		free_objects_memory(&parser);
 	}
+
+	//ok here we start to move things from parser to useable info 
+	
+	//camera related
+	t_camera	c = camera_construct(WIN_WIDTH, WIN_HEIGHT, (parser.camera.fov * (PI / 180)));
+	t_tuple	from = parser.camera.view_point;
+	t_tuple	to = tuple_add(from, parser.camera.orientation_vector);
+	t_tuple	up = tuple_vec(0, 1, 0);
+	pixel_size(&c);
+	c.transform = view_transform(from, to, up);
+	matrix_det_4(&c.transform);
+	c.trans_inverse = matrix_inverse(&c.transform);
+	w.c = c;
+
+	//lighting
+	w.point_light.position = parser.light.light_point;
+	//remember to include brightness
+	w.point_light.intensity = colour_set(1, 1, 1);
+
+	init_mlx(&data, c, &w);
 	return (1);
 }
